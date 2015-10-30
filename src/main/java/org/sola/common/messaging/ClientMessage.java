3874c1b960ae7d29579cdc9796925b32b1ce0b69
Mariapaola Rizzo <mariapaola.rizzo@gmail.com>
2015-10-30 13:50:48 +0100

    changed pom added data folder added create_database.command

--- src/main/java/org/sola/common/messaging/ClientMessage.java
+++ src/main/java/org/sola/common/messaging/ClientMessage.java
@@ -1176,11 +1176,11 @@ public class ClientMessage {
      */
     public static final String ADMIN_FLUSH_CACHE = ADMIN + "036";
      /**
-     * cliadmin037 - Test v1510devL
+     * cliadmin037 - Test v1511a
      */
     public static final String ADMIN_TEST_VERSION_NUMBER = ADMIN + "037";
     /**
-     * cliadmin037 - Release v1510devL
+     * cliadmin037 - Release v1511a
      */
     public static final String ADMIN_PROD_VERSION_NUMBER = ADMIN + "038";
 

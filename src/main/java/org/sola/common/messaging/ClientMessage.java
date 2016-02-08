d509b17c148cc6df03de4fa7fd5f33582cdc0b64
Mariapaola Rizzo <mariapaola.rizzo@gmail.com>
2016-02-08 09:48:34 +0100

    updated pom versions to 1601

--- src/main/java/org/sola/common/messaging/ClientMessage.java
+++ src/main/java/org/sola/common/messaging/ClientMessage.java
@@ -1182,11 +1182,11 @@ public class ClientMessage {
      */
     public static final String ADMIN_FLUSH_CACHE = ADMIN + "036";
      /**
-     * cliadmin037 - Test v1512a
+     * cliadmin037 - Test v1601
      */
     public static final String ADMIN_TEST_VERSION_NUMBER = ADMIN + "037";
     /**
-     * cliadmin037 - Release v1512a
+     * cliadmin037 - Release v1601
      */
     public static final String ADMIN_PROD_VERSION_NUMBER = ADMIN + "038";
 

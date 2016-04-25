96bdf29a24c0d032307403a84d6eb2591cb2de2c
Mariapaola Rizzo <mariapaola.rizzo@gmail.com>
2016-04-25 19:41:31 +0200

    screen resolution

--- src/main/java/org/sola/common/messaging/ClientMessage.java
+++ src/main/java/org/sola/common/messaging/ClientMessage.java
@@ -1187,11 +1187,11 @@ public class ClientMessage {
      */
     public static final String ADMIN_FLUSH_CACHE = ADMIN + "036";
      /**
-     * cliadmin037 - Test v1604c
+     * cliadmin037 - Test v1604e
      */
     public static final String ADMIN_TEST_VERSION_NUMBER = ADMIN + "037";
     /**
-     * cliadmin037 - Release v1604c
+     * cliadmin037 - Release v1604e
      */
     public static final String ADMIN_PROD_VERSION_NUMBER = ADMIN + "038";
 

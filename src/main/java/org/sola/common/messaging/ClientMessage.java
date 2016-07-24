703ff07ac4403f91f284e812fa8ef56d25a0487a
unknown <RizzoM@NRC016.hq.un.fao.org>
2016-07-24 07:32:23 +0200

    fixed LH ticket 45

--- src/main/java/org/sola/common/messaging/ClientMessage.java
+++ src/main/java/org/sola/common/messaging/ClientMessage.java
@@ -1199,11 +1199,11 @@ public class ClientMessage {
      */
     public static final String ADMIN_FLUSH_CACHE = ADMIN + "036";
      /**
-     * cliadmin037 - Test v1607e
+     * cliadmin037 - Test v1607f
      */
     public static final String ADMIN_TEST_VERSION_NUMBER = ADMIN + "037";
     /**
-     * cliadmin037 - Release v1607e
+     * cliadmin037 - Release v1607f
      */
     public static final String ADMIN_PROD_VERSION_NUMBER = ADMIN + "038";
 
